8d67c1a54b88ee444a9040bbf5b815a385758ec9
Wanfranklin Alves <wanfranklin@gmail.com>
2024-02-26 20:57:11 -0300

    atividade aula 02 de programação de sistemas computacionais.

---
+++ ACAD/PSCOM/PSCOM02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Escreva um programa que verifica se um número é par e maior que 10.
+
+int numero = 12;
+
+if(numero % 2 == 0 && numero > 10)
+{
+    Console.WriteLine("O número é par e maior do que 10.");
+}
+else
+{
+    Console.WriteLine("O número não satisfaz as condições.");
+}
