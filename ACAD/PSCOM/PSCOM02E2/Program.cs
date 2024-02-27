cc6124ced564e154b006ac8df80bef08da36fc62
Wanfranklin Alves <wanfranklin@gmail.com>
2024-02-26 21:09:15 -0300

    atividade 02 da aula de programação de soluções computacionais

---
+++ ACAD/PSCOM/PSCOM02E2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Escreva um programa que verifica se 
+    uma pessoa é elegível para votar 
+    (idade maior ou igual a 18 anos) ou se é 
+    menor de idade.
+*/
+
+int idade = 20;
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é elegível para votar.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade e não pode votar.");
+}
