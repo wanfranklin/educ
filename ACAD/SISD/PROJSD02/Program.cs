a7a77916872f10c4cdd9fa75673e28e4e95db87d
Wanfranklin Alves <wanfranklin@gmail.com>
2024-02-29 21:00:19 -0300

    exemplo simples que envia uma solicitação HTTP para um URL especificado e imprime a resposta no console

---
+++ ACAD/SISD/PROJSD02/Program.cs
@@ -0,0 +1,13 @@
+﻿using var httpClient = new HttpClient();
+string url = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"; // Exemplo de URL
+HttpResponseMessage response = await httpClient.GetAsync(url);
+
+if (response.IsSuccessStatusCode)
+{
+    string responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+}
+else
+{
+    Console.WriteLine($"Failed to fetch data. Status code: {response.StatusCode}");
+}
