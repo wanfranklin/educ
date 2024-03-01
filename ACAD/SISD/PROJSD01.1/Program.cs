78ae57c1ea585c1e214afc732255f4d533b9de49
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-01 20:47:40 -0300

    exemplo de conexão com sites através do http

---
+++ ACAD/SISD/PROJSD01.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using var httpClient = new HttpClient();
+string url = "https://google.com"; // Exemplo de URL
+HttpResponseMessage resposta = await httpClient.GetAsync(url);
+
+if (resposta.IsSuccessStatusCode)
+{
+    string corpoResposta = await resposta.Content.ReadAsStringAsync();
+    Console.WriteLine(corpoResposta);
+}
+else
+{
+    Console.WriteLine($"Não foi possível realizar conexão. Status code: {resposta.StatusCode}");
+}
