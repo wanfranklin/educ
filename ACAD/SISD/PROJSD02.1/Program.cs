26e362b9be2cb7a98f1b7aa1b61906ad59d71d9a
Wanfranklin Alves <wanfranklin@gmail.com>
2024-02-29 21:26:01 -0300

    exemplo que faz o download de um arquivo de um servidor FTP e o salva localmente.

---
+++ ACAD/SISD/PROJSD02.1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+string ftpUrl = "https://ftp.unicamp.br/pub/postfix/official/"; // URL do arquivo FTP
+string downloadPath = "postfix-3.8.5.tar.gz"; // Caminho local para salvar o arquivo
+
+using (WebClient client = new WebClient())
+{
+    client.DownloadFile(ftpUrl, downloadPath);
+    Console.WriteLine("File downloaded successfully.");
+}
