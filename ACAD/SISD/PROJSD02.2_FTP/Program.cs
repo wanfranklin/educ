b5220ec42d13ecaf8715e33cb71c07c75f8bacaf
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-01 21:15:59 -0300

    conexao com ftp para download de arquivo de servidor público

---
+++ ACAD/SISD/PROJSD02.2_FTP/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+string ftpUrl = "ftp://ftp.exemplo.br/pub/arquivo.pdf"; // URL do arquivo FTP
+string downloadPath = "livro.pdf"; // Caminho local para salvar o arquivo
+
+using (WebClient client = new WebClient())
+{
+    client.DownloadFile(ftpUrl, downloadPath);
+    Console.WriteLine("Download realizado com sucesso.");
+}
