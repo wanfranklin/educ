d7394966df27ba0ff3e6e1001dd95f250e973418
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-04 21:02:22 -0300

    Código de Exemplo – Todos os tipos de dados em .NET - NET 6 +:

---
+++ ACAD/PSCOM/PSCOM_TIPOSDADOS_03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite o primeiro número:");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int soma = numero1 + numero2;
+
+Console.WriteLine("A soma de " 
++ numero1 + " e " 
++ numero2 + " é igual a: " + soma);
