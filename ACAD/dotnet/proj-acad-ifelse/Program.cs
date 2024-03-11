8a1cd04aede1852b98c4cb83af38176ec2a66e4b
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-11 20:24:52 -0300

    atividade de estrutura de ifelse + ajsute de pastas

---
+++ ACAD/dotnet/proj-acad-ifelse/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int idade = 18;
+
+if (idade <= 17)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você é maior de idade.");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você é menor de idade.");
+    Console.ForegroundColor = ConsoleColor.White;
+}
