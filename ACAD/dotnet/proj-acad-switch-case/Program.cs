6150b63fdd9d07aab5b1373c795132783be4c0d2
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-11 20:42:51 -0300

    criação de atividade com switch

---
+++ ACAD/dotnet/proj-acad-switch-case/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int opcao = 2;
+
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("Opção 1 selecionada.");
+        break;
+    case 2:
+        Console.WriteLine("Opção 2 selecionada.");
+        break;
+    case 3:
+        Console.WriteLine("Opção 3 selecionada.");
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+}
+
+/*
+int op = 1;
+
+switch (op)
+{
+    case 1:
+        // Faça alguma coisa
+        break;
+    case 2:
+        // Faça outra coisa
+        break;
+    default:
+        // Não encontrou o valor da variável, caia aqui
+    break;
+}
+*/
