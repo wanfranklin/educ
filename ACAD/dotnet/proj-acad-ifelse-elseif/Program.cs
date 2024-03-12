3a08dcf8b555e47ec5a028d2a43aebd9de3c2561
Wanfranklin Alves <wanfranklin@gmail.com>
2024-03-11 21:02:39 -0300

    atividade em dotnet para verificar se o número é positivo ou negativo

---
+++ ACAD/dotnet/proj-acad-ifelse-elseif/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite um número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine("O número é positivo.");
+}
+else if (numero < 0)
+{
+    Console.WriteLine("O número é negativo.");
+}
+else
+{
+    Console.WriteLine("O número é zero.");
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.WriteLine();
+Console.ReadKey();
