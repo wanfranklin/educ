961b21637be5aba619d57c608b5c4bb4a732f4e5
Wanfranklin Alves <wanfranklin@gmail.com>
2024-04-18 20:15:18 -0300

    feat: ajuste do modelo com os demais campos de endereço

--- ACAD/dotnet/proj-viacep-planilha-api/Models/ViaCepModel.cs
+++ ACAD/dotnet/proj-viacep-planilha-api/Models/ViaCepModel.cs
@@ -1,5 +1,11 @@
+namespace Models;
+
 public class ViaCepModel
 {
-    public string Cep { get; set; }
-    public string Logradouro { get; set; }
+    public string? Cep { get; set; }
+    public string? Logradouro { get; set; }
+    public string? Complemento { get; set; }
+    public string? Bairro { get; set; }
+    public string? Localidade { get; set; }
+    public string? UF { get; set; }
 }
