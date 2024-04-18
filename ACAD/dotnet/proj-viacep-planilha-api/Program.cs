e51d5c38c8d09f149bb546b73816fdc7f6a80d69
Wanfranklin Alves <wanfranklin@gmail.com>
2024-04-18 20:14:59 -0300

    feat: implementado dados dos demais campos de endereço.

--- ACAD/dotnet/proj-viacep-planilha-api/Program.cs
+++ ACAD/dotnet/proj-viacep-planilha-api/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using ClosedXML.Excel;
 using Newtonsoft.Json;
+using Models;
 
 Console.WriteLine("Digite o CEP: ");
 
@@ -17,13 +18,14 @@ if (response.IsSuccessStatusCode)
     ViaCepModel viaCepModel = JsonConvert.DeserializeObject<ViaCepModel>(responseBody);
     Console.WriteLine("" + responseBody);
 
-    using (var workbook = new XLWorkbook())
-    {
-        var worksheet = workbook.Worksheets.Add("CEP");
-        worksheet.Cells("A1").Value = viaCepModel.Cep;
-        worksheet.Cells("B1").Value = viaCepModel.Logradouro;
-        workbook.SaveAs("Planilha.xlsx");
-
-    }
+    using var workbook = new XLWorkbook();
+    var worksheet = workbook.Worksheets.Add("CEP");
+    worksheet.Cells("A1").Value = viaCepModel.Cep;
+    worksheet.Cells("B1").Value = viaCepModel.Logradouro;
+    worksheet.Cells("C1").Value = viaCepModel.Complemento;
+    worksheet.Cells("D1").Value = viaCepModel.Bairro;
+    worksheet.Cells("E1").Value = viaCepModel.Localidade;
+    worksheet.Cells("F1").Value = viaCepModel.UF;
+    workbook.SaveAs($"Planilha_{DateTime.Now.ToString("ddMMyyyyHHmmss")}.xlsx");
 }
 
